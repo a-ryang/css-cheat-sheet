7380353ce1df08b3d36045929cdae52ac1d1c4bc
a-ryang <chnwu.kim@gmail.com>
2023-08-28 16:12:58 +0900

    GradientImage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { GradientImage } from "@/components/gradient-image";
+
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <GradientImage
+        src="https://picsum.photos/200/300"
+        height={160}
+        width={160}
+        alt="image"
+      />
+    </main>
+  );
 }
---
+++ src/components/gradient-image.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+import styles from "./gradient-image.module.css";
+
+interface GradientImageProps {
+  src: string;
+  height: number;
+  width: number;
+  alt: string;
+}
+
+export function GradientImage({ src, height, width, alt }: GradientImageProps) {
+  return (
+    <div className={styles.container}>
+      <Image src={src} height={height} width={width} alt={alt} />
+    </div>
+  );
+}
